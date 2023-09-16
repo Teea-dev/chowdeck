a02dbd6c91b629fe5ed6b6efd97539de16890937
Adetokunbo Ogunnoiki <adetokunboogunnoiki28@gmail.com>
2023-09-16 09:07:56 +0100

    progress

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import {View,Text,FlatList,StyleSheet,Image,Pressable} from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Image,
+  Pressable,
+} from "react-native";
 
-const Categories = ({navigation}) => {
-    return (
-        <View >
-            <Text>
-                Categories
-            </Text>
-          
-            {/* <FlatList
-                data={DATA}
-                renderItem={({item}) => (
-                    <Pressable onPress={() => navigation.navigate('Details',{item})}>
-                        <Image style={styles.image} source={{uri: item.image}}/>
-                    </Pressable>
-                )}
-                keyExtractor={item => item.id}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-            /> */}
-        </View>
-    );
+const Categories = ({ navigation }) => {
+  const category = [
+    "Resturant",
+    "Pharmacies",
+    "Supermarkets",
+    "African",
+    "More",
+  ];
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={styles.flatlist}
+        data={category}
+        numColumns={2}
+        renderItem={({ item, index }) => (
+          <Pressable
+            style={[
+              styles.categoryContainer,
+              { backgroundColor: getColor(index) },
+            ]}
+          >
+            <Text style={styles.category}>{item}</Text>
+          </Pressable>
+        )}
+        keyExtractor={(item, index) => index.toString}
+      />
+    </View>
+  );
 };
 
+const getColor = (index) => {
+  const colors = ["#83829A", "#FF5733", "#33FF57", "#5733FF", "#FF5733"];
+  return colors[index % colors.length];
+};
 export default Categories;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#0000",
+    margin: 20,
+  },
+  flatlist: {
+    flexDirection: "row",
+  },
+  categoryContainer: {
+    flex: 1,
+    margin: 5,
+  },
+  category: {
+    padding: 30,
+  },
+});
