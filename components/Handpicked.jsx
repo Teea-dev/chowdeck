71221fac2731c75d52315d65d8775f0ead13dfe6
Adetokunbo Ogunnoiki <adetokunboogunnoiki28@gmail.com>
2023-09-17 03:40:56 +0100

    i hope these images work out

--- components/Handpicked.jsx
+++ components/Handpicked.jsx
@@ -1,29 +1,63 @@
-import React from 'react';
-import {View,Text,FlatList,StyleSheet,Image,Pressable} from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Image,
+  Pressable,
+} from "react-native";
+import { COLORS } from "../constants";
 
-const Handpicked = ({navigation}) => {
-
-    const handpickedResturant=['Hexagon Rice','KFC-Ibadan',"Chef-kabs","Burger-King",'Starbucks']
-    return (
-        <View style={styles.container} >
-            <Text >Handpicked for you 💚</Text>
-            <FlatList
-                data={handpickedResturant}
-                renderItem={({item}) => (
-                    <Pressable onPress={() => navigation.navigate('Details',{item})}>
-                        <Image  source={{uri: item.image}}/>
-                        <Text>{item}</Text>
-                    </Pressable>
-                )}
-                keyExtractor={item => item.id}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-            />
-        </View>
-    );
+const Handpicked = ({ navigation }) => {
+  const handpickedResturant = [
+    "Hexagon Rice",
+    "KFC-Ibadan",
+    "Chef-kabs",
+    "Burger-King",
+    "Starbucks",
+    "Dodo-pizza",
+    "Chicken Republic"
+  ];
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Handpicked for you 💚</Text>
+      <FlatList
+        data={handpickedResturant}
+        style={styles.flatlist}
+        renderItem={({ item }) => (
+          <Pressable
+            style={[styles.card, { backgroundColor: COLORS.gray2 }]}
+            onPress={() => navigation.navigate("Details", { item })}
+          >
+            <Image source={{ uri: item.image }} />
+            <Text>{item}</Text>
+          </Pressable>
+        )}
+        keyExtractor={(item) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
 };
 
 export default Handpicked;
 const styles = StyleSheet.create({
-
-})
+  container: {
+    margin: 10,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight:"600",
+    marginLeft:13
+  },
+  card: {
+    margin: 5,
+    padding: 20,
+  },
+  flatlist: {
+    marginVertical:10,
+    marginLeft:3
+  },
+});
