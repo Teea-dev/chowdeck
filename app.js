f5813149f912c7583bcad335fbfda430b8c0f1a2
Adetokunbo Ogunnoiki <adetokunboogunnoiki28@gmail.com>
2023-10-10 18:56:55 +0100

    installed bottom navigator

---
+++ app.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Import your screens here
+import HomeScreen from './screens/HomeScreen';
+import SearchScreen from './screens/SearchScreen';
+import OrdersScreen from './screens/OrdersScreen';
+import SupportScreen from './screens/SupportScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
+// Create a Stack Navigator for each tab
+const HomeStack = createStackNavigator();
+const SearchStack = createStackNavigator();
+const OrdersStack = createStackNavigator();
+const SupportStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
+
+// Create a Bottom Tab Navigator
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Search" component={SearchStackScreen} />
+        <Tab.Screen name="Orders" component={OrdersStackScreen} />
+        <Tab.Screen name="Support" component={SupportStackScreen} />
+        <Tab.Screen name="Profile" component={ProfileStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
